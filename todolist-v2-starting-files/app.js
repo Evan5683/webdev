1cbb59160df495f1424bd466b7d5eb26539c0ea4
Evan5683 <104410745+Evan5683@users.noreply.github.com>
2023-06-01 22:14:21 +1000

    Update app.js
    fix redirect and add more list function

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -31,6 +31,13 @@ const item3 = new Item({ name: "Welcome to your todolist333" });
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", function (req, res) {
   Item
     .find({})
@@ -64,7 +71,7 @@ app.post("/", function (req, res) {
   const item = new Item({ name: itemName });
 
   item.save();
-
+  res.redirect("/");
 });
 
 app.post("/delete", function (req, res) {
@@ -78,6 +85,39 @@ app.post("/delete", function (req, res) {
     .catch((err) => {
       console.log(err);
     });
+
+  res.redirect("/");
+})
+
+
+
+app.get("/:customListName", function (req, res) {
+  const customListName = req.params.customListName;
+
+  List.findOne({ name: customListName })
+    .then(foundList => {
+      if (foundList) {
+        console.log("List exists:", foundList);
+        // 处理列表存在的情况
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items })
+
+
+      } else {
+        console.log("List does not exist");
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        // 处理列表不存在的情况
+      }
+    })
+    .catch(err => {
+      console.log("Error:", err);
+      // 处理错误情况
+    });
+
+
 })
 
 app.get("/work", function (req, res) {
