dda9247435923797cf76ba84a49f3ceecc175194
Evan5683 <104410745+Evan5683@users.noreply.github.com>
2023-06-02 19:14:40 +1000

    rendering database items
    rendering database items

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -2,47 +2,82 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
 
-app.get("/", function(req, res) {
+// item schema
+const itemsSchema = {
+  name: String
+};
 
-const day = date.getDate();
+const Item = mongoose.model("Item", itemsSchema);
 
-  res.render("list", {listTitle: day, newListItems: items});
-
+const item1 = new Item({
+  name: "Item111111"
 });
 
-app.post("/", function(req, res){
+const item2 = new Item({
+  name: "Item22222"
+});
+
+const item3 = new Item({
+  name: "Item33333"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
+app.get("/", function (req, res) {
+  Item.find()
+    .then(foundItems => {
+      if (foundItems.length === 0) {
+        // Insert defaultItems only if no items found
+        Item.insertMany(defaultItems)
+          .then(insertedItems => {
+            console.log("Success insert:", insertedItems);
+            res.redirect("/");
+          })
+          .catch(error => {
+            console.log("Error inserting items:", error);
+            res.status(500).send("Error inserting items");
+          });
+      } else {
+        console.log("Found items:", foundItems);
+        res.render("list", { listTitle: "Today", newListItems: foundItems });
+      }
+    })
+    .catch(error => {
+      console.log("Error finding items:", error);
+      res.status(500).send("Error finding items");
+    });
+});
+
+
+app.post("/", function (req, res) {
 
   const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
