a69ebe38ce4c5183569224ca7c822d9b78ab8a55
Evan5683 <104410745+Evan5683@users.noreply.github.com>
2023-06-15 08:59:47 +1000

    react js
    use map to read note.js

--- keeper-app-part-1-starting/src/components/App.jsx
+++ keeper-app-part-1-starting/src/components/App.jsx
@@ -2,15 +2,36 @@ import React from "react";
 import Heading from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../note";
+
+
 
 function App() {
     return (
         <div>
             <Heading />
-            <Note />
+            {notes.map(noteItem => (
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            ))}
             <Footer />
         </div>
     );
 }
 
 export default App;
+
+
+// function createNotes(noteItem) {
+//     return (
+//         <Note
+//             key={noteItem.key}
+//             title={noteItem.title}
+//             content={noteItem.content}
+
+//         />
+//     );
+// }
